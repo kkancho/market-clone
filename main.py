5a38f8b16d471de38167352d8959cf047ce20f3c
kkancho <lkc910619@gmail.com>
2025-01-20 22:19:15 +0900

    재저장

--- main.py
+++ main.py
@@ -1,7 +1,46 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, Form
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from fastapi.staticfiles import StaticFiles
+from typing import Annotated
+import sqlite3
 
+# Database connection
+con = sqlite3.connect('db.db', check_same_thread=False)
+cur = con.cursor()
+
+# Initialize FastAPI
 app = FastAPI()
 
-app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
+@app.post('/items')
+async def create_item(
+    image: UploadFile,
+    title: Annotated[str, Form()],
+    price: Annotated[int, Form()],
+    description: Annotated[str, Form()],
+    place: Annotated[str, Form()],
+    insertAt: Annotated[int, Form()]
+):
+    image_bytes = await image.read()
 
+    cur.execute(f"""
+                INSERT INTO 
+                items(title, image, price, description, place, insertAt)
+                VALUES ('{title}','{image_bytes.hex}',{price},'{description}','{place}',{insertAt})
+                """)
+
+    con.commit()
+    return '200'
+
+@app.get('/items')
+async def get_items():
+    cur.row_factory = sqlite3.Row
+    cur = con.cursor()
+    rows = cur.execute(f"""
+                       SELECT * FROM items;
+                       """).fetchall()
+
+    return JSONResponse(jsonable_encoder(dict(row) for row in rows))
+
+# Mount static files for frontend
+app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
